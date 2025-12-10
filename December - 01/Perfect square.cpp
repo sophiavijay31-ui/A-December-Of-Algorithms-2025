ec5faf23ceefb9c2d2192e83d01398d82dbcfba0
Sophie <sophiavijay31@gmail.com>
2025-12-10 22:02:08 +0530

    Add files via upload

---
+++ December - 01/Perfect square.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int num,a=0,x,y;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    for(x=1;x<=num;x++)
+    {
+    for(y=1;y<=i;y++)
+    {
+        if(sqrt(x)==y)
+        {
+        printf(" %d",x);
+        a++;
+        }
+    }
+    break;
+    }
+printf("\n%d",a);
+}
